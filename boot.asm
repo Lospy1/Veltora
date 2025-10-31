64fca1f1326cacded3ac9221aea9026c5682d033
Lospy1 <emreyunus6170@gmail.com>
2025-10-31 21:49:23 +0300

    boot.asm

---
+++ boot.asm
@@ -0,0 +1,34 @@
+
+---
+
+## boot.asm
+
+```asm
+org 0x7C00
+
+mov ax, 0xB800
+mov es, ax
+xor di, di
+
+mov cx, 2000
+mov al, ' '
+mov ah, 0x1F
+rep stosw
+
+mov si, msg
+mov di, 80*6 + 12*2
+.next_char:
+    lodsb
+    cmp al,0
+    je .done
+    mov ah,0x1F
+    stosw
+    jmp .next_char
+.done:
+
+jmp $
+
+msg db "Hello GUI Kernel!",0
+
+times 510-($-$$) db 0
+dw 0xAA55
