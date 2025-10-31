a51120792aee6b938e82c06a9556ce97dbee2e1b
Lospy1 <emreyunus6170@gmail.com>
2025-10-31 22:07:22 +0300

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -1,1 +1,35 @@
 
+---
+
+## boot.asm (a)
+
+```asm
+org 0x7C00
+
+mov ax, 0xB800
+mov es, ax
+xor di, di
+
+mov cx, 2000
+mov al, ' '
+mov ah, 0x1F
+rep stosw
+
+mov si, msg
+mov di, 80*6 + 12*2
+.next_char:
+    lodsb
+    cmp al,0
+    je .done
+    mov ah,0x1F
+    stosw
+    jmp .next_char
+.done:
+
+jmp $
+
+msg db "Hello GUI Kernel!",0
+
+times 510-($-$$) db 0
+dw 0xAA55
+
