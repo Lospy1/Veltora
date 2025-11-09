e04da9dfef83488dded6d4405dbd8f9af33f5178
Lcip <emreyunus6170@gmail.com>
2025-11-09 21:33:42 +0300

    Update build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-# Veltora OS Build Script
-# Bu script bootloader'ı derler ve build klasörüne çıktı verir.
+
+echo "=== Veltora OS Build System ==="
+echo "Building from: $(pwd)"
+
+# Bootloader'ı derle
+echo "Building bootloader..."
+nasm -f bin src/boot.asm -o boot.bin
+
+if [ $? -ne 0 ]; then
+    echo "Bootloader build failed!"
+    exit 1
+fi
+
+
+echo "Creating disk image..."
+dd if=/dev/zero of=veltora.img bs=512 count=2880
+dd if=boot.bin of=veltora.img conv=notrunc
+
+rm -f boot.bin
+
+echo "Build successful!"
+echo "Run with: qemu-system-i386 -fda veltora.img"
